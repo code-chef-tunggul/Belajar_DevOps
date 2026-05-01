f1824945c1ebf87392be0557e279c4f252ef037e
Tunggul Dewo <chefdewosushi@gmail.com>
2026-05-01 11:39:01 +0200

    feat: first successful shell script automation

--- salam.sh
+++ salam.sh
@@ -1,18 +1,3 @@
-#!bin/bash
-echo "Halo Chef Dewo!"
-echo "Hari ini tanggal: $(date)
-echo "Semangat belajar DevOps di Zagrreb!"
 #!/bin/bash
-
-# Ini adalah variabel
-NAMA="Chef Dewo"
-KOTA="Zagreb"
-
-echo "Halo $NAMA. selamat bekerja di $KOTA!"
-
-#!/bin/bash
-
-echo "Siapa nama kamu?"
-read NAMA_USER
-
-echo "Halo $NAMA_USER, selamat belajar DevOps"
+echo "Halo Chef Tunggul, Otomatisasi Berhasil!"
+date
